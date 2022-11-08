50729de7bcfe0a85fe431777c87b549135f2d3c6
burntato <kineticz300@gmail.com>
2022-11-08 17:51:39 +0700

    Register fix(?)

--- DTSApp/app/src/main/java/polinema/ac/id/dtsapp/WelcomeSlideCalendarActivity.java
+++ DTSApp/app/src/main/java/polinema/ac/id/dtsapp/WelcomeSlideCalendarActivity.java
@@ -21,4 +21,44 @@ public class WelcomeSlideCalendarActivity extends AppCompatActivity {
         Intent i = new Intent(WelcomeSlideCalendarActivity.this, WelcomeBackActivity.class);
         startActivity(i);
     }
+
+    public void clickContactUs(View view) {
+        // normal
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"dts@polinema.ac.id"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Test Email");
+        i.putExtra(Intent.EXTRA_TEXT, "Welcome to dts 2019");
+        startActivity(i.createChooser(i, "Pilih email client"));
+
+        // LATIHAN
+
+        // ke banyak email ( tinggal di tambahkan aja email di array EXTRA_MAIL )
+        // Intent i = new Intent(Intent.ACTION_SEND);
+        // i.setType("text/plain");
+        // i.putExtra(Intent.EXTRA_EMAIL, new String[]{"dts@polinema.ac.id", "fiandra18@gmail.com"});
+        // i.putExtra(Intent.EXTRA_SUBJECT, "Test Email");
+        // i.putExtra(Intent.EXTRA_TEXT, "Welcome to dts 2019");
+        // startActivity(i.createChooser(i, "Pilih email client"));
+
+        // cc
+        // Intent i = new Intent(Intent.ACTION_SEND);
+        // i.setType("text/plain");
+        // i.putExtra(Intent.EXTRA_EMAIL, new String[]{"dts@polinema.ac.id", "fiandra18@gmail.com"});
+        // i.putExtra(Intent.EXTRA_CC, new String[]{"Poggerinos", "Poggers", "Pogchamp"});
+        // i.putExtra(Intent.EXTRA_SUBJECT, "Test Email");
+        // i.putExtra(Intent.EXTRA_TEXT, "Welcome to dts 2019");
+        // startActivity(i.createChooser(i, "Pilih email client"));
+
+        // bcc
+        // Intent i = new Intent(Intent.ACTION_SEND);
+        // i.setType("text/plain");
+        // i.putExtra(Intent.EXTRA_EMAIL, new String[]{"dts@polinema.ac.id", "fiandra18@gmail.com"});
+        // i.putExtra(Intent.EXTRA_CC, new String[]{"Poggerinos", "Poggers", "Pogchamp"});
+        // i.putExtra(Intent.EXTRA_BCC, new String[]{"Jalapenos", "Tacos", "Quesadila"});
+        // i.putExtra(Intent.EXTRA_SUBJECT, "Test Email");
+        // i.putExtra(Intent.EXTRA_TEXT, "Welcome to dts 2019");
+        // startActivity(i.createChooser(i, "Pilih email client"));
+
+    }
 }
